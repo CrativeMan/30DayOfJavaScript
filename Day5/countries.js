41c13150e6479751fdd508306236f430ce15cb94
CrativeMan <hannig.sebi@gmail.com>
2024-02-03 16:55:40 +0100

    Finished day 5

---
+++ Day5/countries.js
@@ -0,0 +1,17 @@
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+  "test",
+]
+
+module.exports = {
+  countries,
+}
